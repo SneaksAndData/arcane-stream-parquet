54fb4ce789515c17c149cdc7da0472801bb06e8f
George Zubrienko <gzu@ecco.com>
2026-07-14 15:29:14 +0200

    Fix backfill performance

--- src/test/scala/tests/IntegrationTests.scala
+++ src/test/scala/tests/IntegrationTests.scala
@@ -131,6 +131,8 @@ object IntegrationTests extends ZIOSpecDefault:
        |  "source": {
        |    "configuration": {
        |      "sourcePath": "s3a://s3-blob-reader",
+       |      "shardStoragePath": "s3a://tmp",
+       |      "shardSize": 1000,
        |      "tempStoragePath": "/tmp",
        |      "primaryKeys": ["col0"],
        |      "useNameMapping": false,
